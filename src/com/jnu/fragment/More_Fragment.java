0f4a6a8d41437d0d213aa9e9689161c17ca7b43c
英豪 <@5star>
2015-07-29 11:23:35 +0800

    第四页 补充

--- src/com/jnu/fragment/More_Fragment.java
+++ src/com/jnu/fragment/More_Fragment.java
@@ -1,18 +1,24 @@
 package com.jnu.fragment;
 
+import com.jnu.adapter.listview_more_adapter;
+import com.jnu.listenner.more_listview_item_listenner;
+import com.jnu.tilapia.R;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
 
 public class More_Fragment extends Fragment{
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		TextView textView=new TextView(getActivity());
-		textView.setText("尚未完成");
-		return textView;
+		View view=inflater.inflate(R.layout.listview_more, container, false);
+		ListView listView=(ListView) view.findViewById(R.id.list_view_more);
+		listView.setAdapter(new listview_more_adapter(getActivity()));
+		listView.setOnItemClickListener(new more_listview_item_listenner());
+		return view;
 	}
 }
